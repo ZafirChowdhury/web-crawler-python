5e44fa343c423e64f5955348489234b4cc67707c
ZafirChowdhury <zafirchowdhury69@gmail.com>
2025-10-16 16:05:44 +0600

    Update Tests

--- test_crawl.py
+++ test_crawl.py
@@ -5,18 +5,18 @@ from crawl import normalize_url, get_h1_from_html, get_first_paragraph_from_html
 
 class TestCrawl(unittest.TestCase):
     def test_normalize_url(self):
-        expected = "blog.boot.dev/path"
+        input_output_map = {
+            "https://blog.boot.dev/path/": "blog.boot.dev/path",
+            "https://blog.boot.dev/path": "blog.boot.dev/path",
+            "http://blog.boot.dev/path/": "blog.boot.dev/path",
+            "http://blog.boot.dev/path": "blog.boot.dev/path",
 
-        test_cases = [
-        "https://blog.boot.dev/path/",
-        "https://blog.boot.dev/path",
-        "http://blog.boot.dev/path/",
-        "http://blog.boot.dev/path",
-        ]
+        }
 
-        for input_url in test_cases:
-            with self.subTest(input_url=input_url):
-                self.assertEqual(normalize_url(input_url), expected)
+        for test_case, expected_output in input_output_map.items():
+            with self.subTest(test_case=test_case):
+                self.assertEqual(normalize_url(test_case), expected_output)
+
 
     def test_get_h1_from_html(self):
         # key: value = test_input: expected_output
@@ -29,15 +29,34 @@ class TestCrawl(unittest.TestCase):
             '<h1 class="big">Spicy</h1>': "Spicy",
             " \n <h1>\n spaced \n</h1> ": "spaced",
             "<h1>first</h1><h1>second</h1>": "first",
+            "<html><body><h1>Test Title</h1></body></html>": "Test Title",
         }
 
         for test_case, expected_output in input_output_map.items():
             with self.subTest(test_case=test_case):
                 self.assertEqual(get_h1_from_html(test_case), expected_output)
 
+
     def test_get_first_paragraph_from_html(self):
-        pass
-        
-        
+        input_output_map = {
+            '''<html><body>
+                <p>Outside paragraph.</p>
+                <main>
+                    <p>Main paragraph.</p>
+                </main>
+            </body></html>''' : "Main paragraph.",
+
+            '''<html><body>
+                <p>Outside paragraph.</p>
+                <main>
+                </main>
+            </body></html>''' : "Outside paragraph."
+        }
+
+        for test_case, expected_output in input_output_map.items():
+            with self.subTest(test_case=test_case):
+                self.assertEqual(get_first_paragraph_from_html(test_case), expected_output)
+
+           
 if __name__ == "__main__":
     unittest.main()
