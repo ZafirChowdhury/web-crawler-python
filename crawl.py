189c8977a912f3efbd7fecadf61a1d696a629799
ZafirChowdhury <zafirchowdhury69@gmail.com>
2025-10-16 14:15:09 +0600

    Add get_h1 and test

--- crawl.py
+++ crawl.py
@@ -1,4 +1,24 @@
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def normalize_url(url):
+    parsed_url = urlparse(url)
+
+    path = parsed_url.path
+
+    while path.endswith("/"):
+        path = path[:-1]
+
+    return f"{parsed_url.netloc}{path}"
+
+def get_h1_from_html(html):
+    parsed_html = BeautifulSoup(html, "html.parser")
+
+    h1 = parsed_html.find("h1")
+    if h1:
+        return h1.get_text().strip()
+    
+    return ""
+
+def get_first_paragraph_from_html(html):
     pass
--- test_crawl.py
+++ test_crawl.py
@@ -1,5 +1,6 @@
 import unittest
-from crawl import normalize_url
+
+from crawl import normalize_url, get_h1_from_html, get_first_paragraph_from_html
 
 
 class TestCrawl(unittest.TestCase):
@@ -17,5 +18,26 @@ class TestCrawl(unittest.TestCase):
             with self.subTest(input_url=input_url):
                 self.assertEqual(normalize_url(input_url), expected)
 
+    def test_get_h1_from_html(self):
+        # key: value = test_input: expected_output
+        input_output_map = {
+            "<h1>Hello, from boot.dev</h1>": "Hello, from boot.dev",
+            "<h2>This is 2nd heading</h2>": "",
+            "<title>Title</title><h1>To be read h1</h1>": "To be read h1",
+            "<h1></h1>": "",
+            "<h1<h1>": "",  # malformed
+            '<h1 class="big">Spicy</h1>': "Spicy",
+            " \n <h1>\n spaced \n</h1> ": "spaced",
+            "<h1>first</h1><h1>second</h1>": "first",
+        }
+
+        for test_case, expected_output in input_output_map.items():
+            with self.subTest(test_case=test_case):
+                self.assertEqual(get_h1_from_html(test_case), expected_output)
+
+    def test_get_first_paragraph_from_html(self):
+        pass
+        
+        
 if __name__ == "__main__":
     unittest.main()
